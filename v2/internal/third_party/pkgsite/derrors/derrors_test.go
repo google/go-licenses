dda69a819b9b4032ecfdd0fc739606b4e59f18e8
Yuan Gong <gongyuan94@gmail.com>
2022-01-11 08:35:47 +0000

    rm all unused functions in derrors

--- v2/internal/third_party/pkgsite/derrors/derrors_test.go
+++ v2/internal/third_party/pkgsite/derrors/derrors_test.go
@@ -6,80 +6,8 @@ package derrors
 
 import (
 	"errors"
-	"fmt"
-	"io"
-	"net/http"
 	"testing"
 )
-
-func TestFromHTTPStatus(t *testing.T) {
-	tests := []struct {
-		label  string
-		status int
-		want   error
-	}{
-		{
-			label:  "OK translates to nil error",
-			status: 200,
-		},
-		{
-			label:  "400 translates to invalid argument",
-			status: 400,
-			want:   InvalidArgument,
-		},
-		// Testing other specific HTTP status codes is intentionally omitted to
-		// avoid writing a change detector.
-	}
-
-	for _, test := range tests {
-		test := test
-		t.Run(test.label, func(t *testing.T) {
-			err := FromStatus(test.status, "error")
-			if !errors.Is(err, test.want) {
-				t.Errorf("FromHTTPStatus(%d, ...) = %v, want %v", test.status, err, test.want)
-			}
-		})
-	}
-}
-
-func TestToHTTPStatus(t *testing.T) {
-	for _, test := range []struct {
-		in   error
-		want int
-	}{
-		{nil, http.StatusOK},
-		{InvalidArgument, http.StatusBadRequest},
-		{NotFound, http.StatusNotFound},
-		{BadModule, 490},
-		{AlternativeModule, 491},
-		{Unknown, http.StatusInternalServerError},
-		{fmt.Errorf("wrapping: %w", NotFound), http.StatusNotFound},
-		{io.ErrUnexpectedEOF, http.StatusInternalServerError},
-	} {
-		got := ToStatus(test.in)
-		if got != test.want {
-			t.Errorf("ToHTTPStatus(%v) = %d, want %d", test.in, got, test.want)
-		}
-	}
-}
-
-func TestAdd(t *testing.T) {
-	var err error
-	Add(&err, "whatever")
-	if err != nil {
-		t.Errorf("got %v, want nil", err)
-	}
-
-	err = errors.New("bad stuff")
-	Add(&err, "Frob(%d)", 3)
-	want := "Frob(3): bad stuff"
-	if got := err.Error(); got != want {
-		t.Errorf("got %s, want %s", got, want)
-	}
-	if got := errors.Unwrap(err); got != nil {
-		t.Errorf("Unwrap: got %v, want nil", got)
-	}
-}
 
 func TestWrap(t *testing.T) {
 	var err error
