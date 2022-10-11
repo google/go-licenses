e10d1025f2c8999db85fef1496ce75d683cc500d
ohkinozomu <nozomunoise@gmail.com>
2022-10-11 10:31:55 +0900

    Refactoring E2E test

--- e2e_test.go
+++ e2e_test.go
@@ -29,21 +29,14 @@ import (
 
 var update = flag.Bool("update", false, "update golden files")
 
-func TestReportCommandE2E(t *testing.T) {
-	tests := []struct {
-		workdir        string
-		args           []string // additional arguments to pass to report command.
-		goldenFilePath string
-	}{
-		{"testdata/modules/hello01", nil, "licenses.csv"},
-		{"testdata/modules/cli02", nil, "licenses.csv"},
-		{"testdata/modules/vendored03", nil, "licenses.csv"},
-		{"testdata/modules/replace04", nil, "licenses.csv"},
+type test struct {
+	workdir        string
+	args           []string // additional arguments to pass to command.
+	goldenFilePath string
+	wantExitCode   int
+}
 
-		{"testdata/modules/hello01", []string{"--template", "licenses.tpl"}, "licenses.md"},
-		{"testdata/modules/template01", []string{"--template", "licenses.tpl"}, "licenses.md"},
-	}
-
+func runE2ETest(t *testing.T, tests []test, testFn func(t *testing.T, tt test, originalWorkDir string, goLicensesPath string)) {
 	originalWorkDir, err := os.Getwd()
 	if err != nil {
 		t.Fatal(err)
@@ -75,50 +68,112 @@ func TestReportCommandE2E(t *testing.T) {
 			if err != nil {
 				t.Fatalf("downloading go modules:\n%s", string(log))
 			}
-			args := append([]string{"report", "."}, tt.args...)
-			cmd = exec.Command(goLicensesPath, args...)
-			// Capture stderr to buffer.
-			var stderr bytes.Buffer
-			cmd.Stderr = &stderr
-			t.Logf("%s $ go-licenses report .", tt.workdir)
-			output, err := cmd.Output()
-			if err != nil {
-				t.Logf("\n=== start of log ===\n%s=== end of log ===\n\n\n", stderr.String())
-				t.Fatalf("running go-licenses report: %s. Full log shown above.", err)
-			}
-			got := string(output)
-			if *update {
-				err := os.WriteFile(tt.goldenFilePath, output, 0600)
-				if err != nil {
-					t.Fatalf("writing golden file: %s", err)
-				}
-			}
-			goldenBytes, err := os.ReadFile(tt.goldenFilePath)
-			if err != nil {
-				if errors.Is(err, os.ErrNotExist) {
-					t.Fatalf("reading golden file: %s. Create a golden file by running `go test --update .`", err)
-				}
-				t.Fatalf("reading golden file: %s", err)
-			}
-			golden := string(goldenBytes)
-			if got != golden {
-				t.Logf("\n=== start of log ===\n%s=== end of log ===\n\n\n", stderr.String())
-				t.Fatalf("result of go-licenses report does not match the golden file.\n"+
-					"Diff -golden +got:\n%s\n"+
-					"Update the golden by running `go test --update .`",
-					cmp.Diff(golden, got))
-			}
+			testFn(t, tt, originalWorkDir, goLicensesPath)
 		})
 	}
 }
 
+func runTestReportCommand(t *testing.T, tt test, originalWorkDir string, goLicensesPath string) {
+	args := append([]string{"report", "."}, tt.args...)
+	cmd := exec.Command(goLicensesPath, args...)
+	// Capture stderr to buffer.
+	var stderr bytes.Buffer
+	cmd.Stderr = &stderr
+	t.Logf("%s $ go-licenses report .", tt.workdir)
+	output, err := cmd.Output()
+	if err != nil {
+		t.Logf("\n=== start of log ===\n%s=== end of log ===\n\n\n", stderr.String())
+		t.Fatalf("running go-licenses report: %s. Full log shown above.", err)
+	}
+	got := string(output)
+	if *update {
+		err := os.WriteFile(tt.goldenFilePath, output, 0600)
+		if err != nil {
+			t.Fatalf("writing golden file: %s", err)
+		}
+	}
+	goldenBytes, err := os.ReadFile(tt.goldenFilePath)
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			t.Fatalf("reading golden file: %s. Create a golden file by running `go test --update .`", err)
+		}
+		t.Fatalf("reading golden file: %s", err)
+	}
+	golden := string(goldenBytes)
+	if got != golden {
+		t.Logf("\n=== start of log ===\n%s=== end of log ===\n\n\n", stderr.String())
+		t.Fatalf("result of go-licenses report does not match the golden file.\n"+
+			"Diff -golden +got:\n%s\n"+
+			"Update the golden by running `go test --update .`",
+			cmp.Diff(golden, got))
+	}
+}
+
+func TestReportCommandE2E(t *testing.T) {
+	tests := []test{
+		{"testdata/modules/hello01", nil, "licenses.csv", 0},
+		{"testdata/modules/cli02", nil, "licenses.csv", 0},
+		{"testdata/modules/vendored03", nil, "licenses.csv", 0},
+		{"testdata/modules/replace04", nil, "licenses.csv", 0},
+
+		{"testdata/modules/hello01", []string{"--template", "licenses.tpl"}, "licenses.md", 0},
+		{"testdata/modules/template01", []string{"--template", "licenses.tpl"}, "licenses.md", 0},
+	}
+
+	runE2ETest(t, tests, runTestReportCommand)
+}
+
+func runTestCheckCommand(t *testing.T, tt test, originalWorkDir string, goLicensesPath string) {
+	args := append([]string{"check", "."}, tt.args...)
+	cmd := exec.Command(goLicensesPath, args...)
+	// Capture stderr to buffer.
+	var stderr bytes.Buffer
+	cmd.Stderr = &stderr
+	exitCode := 0
+	t.Logf("%s $ go-licenses check .", tt.workdir)
+	output, err := cmd.Output()
+	if err != nil {
+		exitCode = -1
+
+		if exitError, ok := err.(*exec.ExitError); ok {
+			exitCode = exitError.ExitCode()
+		}
+	}
+
+	if len(output) != 0 {
+		t.Fatalf("unexpected output running go-licenses check: %s.", string(output))
+	}
+
+	if exitCode != tt.wantExitCode {
+		t.Fatalf("unexpected exit code running go-licenses check, expected %d but got %d", tt.wantExitCode, exitCode)
+	}
+
+	got := filterOutput(stderr.String())
+	if *update {
+		err := os.WriteFile(tt.goldenFilePath, []byte(got), 0600)
+		if err != nil {
+			t.Fatalf("writing golden file: %s", err)
+		}
+	}
+	goldenBytes, err := os.ReadFile(tt.goldenFilePath)
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			t.Fatalf("reading golden file: %s. Create a golden file by running `go test --update .`", err)
+		}
+		t.Fatalf("reading golden file: %s", err)
+	}
+	golden := string(goldenBytes)
+	if got != golden {
+		t.Logf("\n=== start of log ===\n%s=== end of log ===\n\n\n", stderr.String())
+		t.Fatalf("result of go-licenses check does not match the golden file.\n"+
+			"Diff -golden +got:\n%s\n"+
+			"Update the golden by running `go test --update .`",
+			cmp.Diff(golden, got))
+	}
+}
+
 func TestCheckCommandE2E(t *testing.T) {
-	tests := []struct {
-		workdir        string
-		args           []string // additional arguments to pass to report command.
-		goldenFilePath string
-		wantExitCode   int
-	}{
+	tests := []test{
 		{"testdata/modules/hello01", nil, "output-check-forbidden.txt", 0},
 		{"testdata/modules/hello01", []string{"--disallowed_types=forbidden,notice"}, "output-check-notice-forbidden.txt", 1},
 		{"testdata/modules/cli02", nil, "output-check-forbidden.txt", 0},
@@ -128,85 +183,7 @@ func TestCheckCommandE2E(t *testing.T) {
 		{"testdata/modules/cli02", []string{"--allowed_licenses= Apache-2.0, MIT"}, "output-check-license-names-2.txt", 1},
 	}
 
-	originalWorkDir, err := os.Getwd()
-	if err != nil {
-		t.Fatal(err)
-	}
-	t.Cleanup(func() { _ = os.Chdir(originalWorkDir) })
-
-	// This builds go-licenses CLI to temporary dir.
-	tempDir, err := os.MkdirTemp("", "")
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer os.RemoveAll(tempDir)
-	goLicensesPath := filepath.Join(tempDir, "go-licenses")
-	cmd := exec.Command("go", "build", "-o", goLicensesPath)
-	_, err = cmd.Output()
-	if err != nil {
-		t.Fatal(err)
-	}
-	t.Logf("Built go-licenses binary in %s.", goLicensesPath)
-
-	for _, tt := range tests {
-		t.Run(tt.workdir, func(t *testing.T) {
-			err := os.Chdir(filepath.Join(originalWorkDir, tt.workdir))
-			if err != nil {
-				t.Fatal(err)
-			}
-			cmd := exec.Command("go", "mod", "download")
-			log, err := cmd.CombinedOutput()
-			if err != nil {
-				t.Fatalf("downloading go modules:\n%s", string(log))
-			}
-			args := append([]string{"check", "."}, tt.args...)
-			cmd = exec.Command(goLicensesPath, args...)
-			// Capture stderr to buffer.
-			var stderr bytes.Buffer
-			cmd.Stderr = &stderr
-			exitCode := 0
-			t.Logf("%s $ go-licenses check .", tt.workdir)
-			output, err := cmd.Output()
-			if err != nil {
-				exitCode = -1
-
-				if exitError, ok := err.(*exec.ExitError); ok {
-					exitCode = exitError.ExitCode()
-				}
-			}
-
-			if len(output) != 0 {
-				t.Fatalf("unexpected output running go-licenses check: %s.", string(output))
-			}
-
-			if exitCode != tt.wantExitCode {
-				t.Fatalf("unexpected exit code running go-licenses check, expected %d but got %d", tt.wantExitCode, exitCode)
-			}
-
-			got := filterOutput(stderr.String())
-			if *update {
-				err := os.WriteFile(tt.goldenFilePath, []byte(got), 0600)
-				if err != nil {
-					t.Fatalf("writing golden file: %s", err)
-				}
-			}
-			goldenBytes, err := os.ReadFile(tt.goldenFilePath)
-			if err != nil {
-				if errors.Is(err, os.ErrNotExist) {
-					t.Fatalf("reading golden file: %s. Create a golden file by running `go test --update .`", err)
-				}
-				t.Fatalf("reading golden file: %s", err)
-			}
-			golden := string(goldenBytes)
-			if got != golden {
-				t.Logf("\n=== start of log ===\n%s=== end of log ===\n\n\n", stderr.String())
-				t.Fatalf("result of go-licenses check does not match the golden file.\n"+
-					"Diff -golden +got:\n%s\n"+
-					"Update the golden by running `go test --update .`",
-					cmp.Diff(golden, got))
-			}
-		})
-	}
+	runE2ETest(t, tests, runTestCheckCommand)
 }
 
 func filterOutput(output string) string {
