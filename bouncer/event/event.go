015a07acc0fdc8e5e1ce80fe42847756204a887e
sulaiman-coder <51925710+sulaiman-coder@users.noreply.github.com>
2023-07-10 23:48:52 -0700

    Create event.go

---
+++ bouncer/event/event.go
@@ -0,0 +1,8 @@
+package event
+
+import "github.com/sulaiman-coder/gopeventbus"
+
+const (
+	ModuleScanStarted peventbus.EventType = "bouncer-module-scan-started"
+	ModuleScanResult  peventbus.EventType = "bouncer-module-scan-result"
+)
