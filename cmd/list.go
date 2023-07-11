862d4feccf1df3b0557e9b632da7fc053aaed0e4
sulaiman-coder <51925710+sulaiman-coder@users.noreply.github.com>
2023-07-10 22:49:12 -0700

    Update list.go

--- cmd/list.go
+++ cmd/list.go
@@ -7,7 +7,6 @@ import (
 	"github.com/spf13/cobra"
 	"github.com/sulaiman-coder/gobouncer/bouncer"
 	"github.com/sulaiman-coder/gobouncer/bouncer/presenter"
-	"github.com/sulaiman-coder/gobouncer/bouncer/testdata/modules"
 )
 
 var gitRemotes []string
