470f619bcf4ba3ddc3561ead68c5c54c7f77bd01
sulaiman-coder <51925710+sulaiman-coder@users.noreply.github.com>
2023-07-10 20:41:42 -0700

    Create presenter.go

---
+++ bouncer/presenter/presenter.go
@@ -0,0 +1,28 @@
+package presenter
+
+import (
+	"io"
+
+	"github.com/sulaiman-coder/gobouncer/bouncer"
+	"github.com/sulaiman-coder/gobouncer/bouncer/presenter/csv"
+	"github.com/sulaiman-coder/gobouncer/bouncer/presenter/json"
+	"github.com/sulaiman-coder/gobouncer/bouncer/presenter/text"
+)
+
+type Presenter interface {
+	Present(io.Writer) error
+}
+
+func GetPresenter(option Option, results <-chan bouncer.LicenseResult) Presenter {
+	switch option {
+	case CSVPresenter:
+		return csv.NewPresenter(results)
+	case JSONPresenter:
+		return json.NewPresenter(results)
+	case TextPresenter:
+		return text.NewPresenter(results)
+
+	default:
+		return nil
+	}
+}
