6b21f559b750a77a4a366594805309fda0131073
Md Sulaiman <51925710+sulaiman-coder@users.noreply.github.com>
2023-10-20 16:38:37 +0600

    Create presenter.go
    Signed-off-by: Md Sulaiman <51925710+sulaiman-coder@users.noreply.github.com>

---
+++ bouncer/presenter/csv/presenter.go
@@ -0,0 +1,29 @@
+package csv
+
+import (
+	"encoding/csv"
+	"io"
+
+	"github.com/khulnasoft/go-bouncer/bouncer"
+)
+
+type Presenter struct {
+	resultStream <-chan bouncer.LicenseResult
+}
+
+func NewPresenter(results <-chan bouncer.LicenseResult) Presenter {
+	return Presenter{
+		resultStream: results,
+	}
+}
+
+func (p Presenter) Present(target io.Writer) error {
+	writer := csv.NewWriter(target)
+	for result := range p.resultStream {
+		if err := writer.Write([]string{result.Library, result.URL, result.Type, result.License}); err != nil {
+			return err
+		}
+	}
+	writer.Flush()
+	return writer.Error()
+}
