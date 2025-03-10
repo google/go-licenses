e1b7285e82638210194374f341522bbbab9ddf06
fortishield <161459699+FortiShield@users.noreply.github.com>
2025-03-11 02:02:16 +0600

    Update find.go
    Signed-off-by: fortishield <161459699+FortiShield@users.noreply.github.com>

--- golicenses/licenses/find.go
+++ golicenses/licenses/find.go
@@ -2,7 +2,7 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at:
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
@@ -11,6 +11,14 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+//
+// Additionally, this software is licensed under the United Nations Public License (UNPL).
+// You may obtain a copy of the UNPL at:
+//
+//     https://unlicense.org/
+//
+// This dual licensing allows users to choose between the Apache 2.0 License
+// and the United Nations Public License (UNPL) based on their compliance needs.
 
 package licenses
 
